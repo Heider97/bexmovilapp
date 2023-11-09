063f4450a71f6aab71cb66be16a118ee7512bc11
BexSebastian <sebastian.monroy@bexsoluciones.com>
2023-11-09 11:21:17 -0500

    changes in witgets

--- lib/src/presentation/widgets/custom_grid_item.dart
+++ lib/src/presentation/widgets/custom_grid_item.dart
@@ -9,55 +9,86 @@ class CustomGridItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomScrollView(
       primary: false,
-      slivers: <Widget>[
+      slivers: [
         SliverPadding(
           padding: const EdgeInsets.all(20),
           sliver: SliverGrid.count(
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
             crossAxisCount: 2,
-            children: <Widget>[
+            children: [
               Card(
                 shape: RoundedRectangleBorder(
                     side: BorderSide(color: Colors.orange),
                     borderRadius: BorderRadius.circular(10)),
                 elevation: 8,
-                child: Text(text),
+                child: Column(
+                  children: [
+                    Icon(Icons.alarm),
+                    Text(text),
+                  ],
+                ),
               ),
               Card(
                 shape: RoundedRectangleBorder(
                     side: BorderSide(color: Colors.orange),
                     borderRadius: BorderRadius.circular(10)),
                 elevation: 8,
-                child: Text(text),
+                child: Column(
+                  children: [
+                    Icon(Icons.alarm),
+                    Text(text),
+                  ],
+                ),
               ),
               Card(
                 shape: RoundedRectangleBorder(
                     side: BorderSide(color: Colors.orange),
                     borderRadius: BorderRadius.circular(10)),
                 elevation: 8,
-                child: Text(text),
+                child: Column(
+                  children: [
+                    Icon(Icons.alarm),
+                    Text(text),
+                  ],
+                ),
               ),
               Card(
                 shape: RoundedRectangleBorder(
                     side: BorderSide(color: Colors.orange),
                     borderRadius: BorderRadius.circular(10)),
                 elevation: 8,
-                child: Text(text),
+                child: Column(
+                  children: [
+                    Icon(Icons.alarm),
+                    Text(text),
+                  ],
+                ),
               ),
               Card(
                 shape: RoundedRectangleBorder(
                     side: BorderSide(color: Colors.orange),
                     borderRadius: BorderRadius.circular(10)),
                 elevation: 8,
-                child: Text(text),
+                child: Column(
+                  children: [
+                    Icon(Icons.alarm),
+                    Text(text),
+                  ],
+                ),
               ),
               Card(
                 shape: RoundedRectangleBorder(
                     side: BorderSide(color: Colors.orange),
                     borderRadius: BorderRadius.circular(10)),
                 elevation: 8,
-                child: Text(text),
+                child: Column(
+                  children: [
+                    
+                    Icon(Icons.alarm),
+                    Text(text),
+                  ],
+                ),
               ),
             ],
           ),
