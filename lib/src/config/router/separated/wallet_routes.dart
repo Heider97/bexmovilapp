fe627a07b3dc107d418a732dd4634192b2e6da56
Heider Zapa <heider.zapa@bexsoluciones.com>
2024-03-01 07:48:26 -0500

    refactor wallet view (organize widgets and views)

--- lib/src/config/router/separated/wallet_routes.dart
+++ lib/src/config/router/separated/wallet_routes.dart
@@ -1,8 +1,9 @@
 //utils
-
 import '../../../utils/constants/strings.dart';
 //router
 import '../route_type.dart';
+//domain
+import '../../../domain/models/arguments.dart';
 //views
 import '../../../presentation/widgets/global/global_background.dart';
 import '../../../presentation/widgets/global/global_background_square.dart';
@@ -10,8 +11,12 @@ import '../../../presentation/views/user/wallet/pages/dashboard.dart';
 import '../../../presentation/views/user/wallet/pages/clients.dart';
 
 Map<String, RouteType> walletRoutes = {
-  AppRoutes.dashboardWallet: (context, arguments) =>
+  AppRoutes.dashboardWallet: (context, settings) =>
       const GlobalBackground(child: WalletDashboardView()),
-  AppRoutes.clientsWallet: (context, arguments) => const GlobalBackgroundSquare(
-      opacity: 0.1, hideBottomNavigationBar: true, child: WalletClientsView()),
+  AppRoutes.clientsWallet: (context, settings) => GlobalBackgroundSquare(
+      opacity: 0.1,
+      hideBottomNavigationBar: true,
+      child: WalletClientsView(
+        walletArgument: settings.arguments as WalletArgument,
+      )),
 };
--- lib/src/presentation/views/user/wallet/pages/clients.dart
+++ lib/src/presentation/views/user/wallet/pages/clients.dart
@@ -1,20 +1,31 @@
-import 'package:bexmovil/src/presentation/blocs/wallet/wallet_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 //utils
+
+import '../../../../../utils/constants/gaps.dart';
 import '../../../../../utils/constants/strings.dart';
 
-//atoms
-import '../../../../widgets/atomsbox.dart';
+//blocs
+import '../../../../blocs/wallet/wallet_bloc.dart';
 
+//domain
+import '../../../../../domain/models/arguments.dart';
+
+//atoms
+import '../../../../blocs/sale_stepper/sale_stepper_bloc.dart';
+import '../../../../widgets/atomsbox.dart';
+import '../../../../widgets/user/stepper.dart';
 
 class WalletClientsView extends StatefulWidget {
-  const WalletClientsView({super.key});
+  final WalletArgument? walletArgument;
+  const WalletClientsView({super.key, this.walletArgument});
 
   @override
   State<WalletClientsView> createState() => _WalletClientsViewState();
 }
+
+late SaleStepperBloc saleStepperBloc;
 
 class _WalletClientsViewState extends State<WalletClientsView> {
   TextEditingController searchController = TextEditingController();
@@ -24,12 +35,35 @@ class _WalletClientsViewState extends State<WalletClientsView> {
   @override
   void initState() {
     walletBloc = BlocProvider.of<WalletBloc>(context);
-    walletBloc.add(LoadGraphics());
+    // walletBloc.add(LoadGraphics());
+    saleStepperBloc = BlocProvider.of(context);
     super.initState();
   }
 
+  List<StepData> steps = [
+    StepData(
+        "Seleccionar \nCliente",
+        'assets/icons/ProfileEnable.png',
+        const Color(0xFFF4F4F4),
+        'assets/icons/ProfileDisable.png',
+        () => saleStepperBloc.add(ChangeStepEvent(index: 0))),
+    StepData(
+        "Seleccionar \n Facturas",
+        'assets/icons/seleccionarFacturaEnable.png',
+        const Color(0xFFF4F4F4),
+        'assets/icons/seleccionarFacturaDisable.png',
+        () => saleStepperBloc.add(ChangeStepEvent(index: 1))),
+    StepData(
+        'Detalles de \n la orden',
+        'assets/icons/actionEnable.png',
+        const Color(0xFFF4F4F4),
+        'assets/icons/actionDisable.png',
+        () => saleStepperBloc.add(ChangeStepEvent(index: 2))),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     return SafeArea(
       child: SingleChildScrollView(
         child: Column(
@@ -40,6 +74,7 @@ class _WalletClientsViewState extends State<WalletClientsView> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const AppBackButton(needPrimary: true),
+                  AppText(widget.walletArgument!.type),
                   AppIconButton(
                       onPressed: null,
                       child: Icon(Icons.menu,
@@ -49,7 +84,30 @@ class _WalletClientsViewState extends State<WalletClientsView> {
                 ],
               ),
             ),
-
+            StepperWidget(currentStep: 0, steps: steps),
+            Padding(
+                padding: const EdgeInsets.all(Const.padding),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: theme.colorScheme.secondary,
+                      borderRadius: BorderRadius.circular(Const.space15)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(Const.padding),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: theme.colorScheme.tertiary,
+                        ),
+                        gapW24,
+                        Text(
+                          'Búsqueda por clientes',
+                          style: TextStyle(color: theme.colorScheme.tertiary),
+                        )
+                      ],
+                    ),
+                  ),
+                )),
           ],
         ),
       ),
