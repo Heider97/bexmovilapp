1a3361b747c1348b25b8b0e5ddd2ce6b3bdb7d97
Heider Zapa <heider.zapa@bexsoluciones.com>
2024-03-04 10:36:48 -0500

    working on filters

---
+++ lib/src/domain/models/option.dart
@@ -0,0 +1,57 @@
+const String tableOptions = 'options';
+
+class OptionFields {
+  static final List<String> values = [
+    id,
+    name,
+    filterId,
+    queryId
+  ];
+
+  static const String id = 'id';
+  static const String name = 'name';
+  static const String filterId = 'filter_id';
+  static const String queryId = 'query_id';
+}
+
+class Option {
+  Option({
+    this.id,
+    this.name,
+    this.filterId,
+    this.queryId,
+  });
+
+  Option copy({
+    int? id,
+    String? name,
+    int? filterId,
+    int? queryId,
+  }) =>
+      Option(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        filterId: filterId ?? this.filterId,
+        queryId: queryId ?? this.queryId,
+      );
+
+  Option.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    filterId = json['filter_id'];
+    queryId = json['query_id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['name'] = name;
+    data['filter_id'] = filterId;
+    data['query_id'] = queryId;
+    return data;
+  }
+
+  int? id;
+  String? name;
+  int? filterId;
+  int? queryId;
+}
