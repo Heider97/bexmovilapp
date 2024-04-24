13125e445932f8e59f85d5a043f041975f1f48e6
Jairo Grande <jairo.grande@bexsoluciones.com>
2024-04-24 02:39:50 -0500

    show clients bug fixed

--- lib/src/presentation/views/user/sale/widgets/custom_draggable_scrollable_sheet.dart
+++ lib/src/presentation/views/user/sale/widgets/custom_draggable_scrollable_sheet.dart
@@ -217,7 +217,7 @@ class _CustomDraggableScrollableSheetState
                           if (state.status == SaleStatus.loading) {
                             return const Center(
                                 child: CircularProgressIndicator());
-                          } else if (state.status == SaleStatus.success &&
+                          } else if (state.status == SaleStatus.showClients &&
                               state.clients!.isNotEmpty) {
                             return SizedBox(
                               height: Screens.height(context) * 0.8,
