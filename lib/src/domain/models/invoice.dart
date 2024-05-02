7200489d54d408becbc42815c93f75bfb5814161
Heider Zapa <heider.zapa@bexsoluciones.com>
2024-05-02 12:19:47 -0500

    add widget to query loader service

--- lib/src/domain/models/invoice.dart
+++ lib/src/domain/models/invoice.dart
@@ -2,12 +2,14 @@ class Invoice {
   int? codcliente;
   int? nummov;
   String? fecven;
+  String? state;
   int? preciomov;
 
   Invoice({
     this.codcliente,
     this.nummov,
     this.fecven,
+    this.state,
     this.preciomov,
   });
 
@@ -16,6 +18,7 @@ class Invoice {
       codcliente: json['codcliente'],
       nummov: json['nummov'],
       fecven: json['fecven'],
+      state: json['state'],
       preciomov: json['preciomov'],
     );
   }
@@ -25,6 +28,7 @@ class Invoice {
       'codcliente': codcliente,
       'fecven': fecven,
       'nummov': nummov,
+      'state' : state,
       'preciomov': preciomov,
     };
   }
