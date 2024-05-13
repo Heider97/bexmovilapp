f809f3fbee6bb84bd1d5f41d7cdad2c5d669a260
Heider Zapa <heider.zapa@bexsoluciones.com>
2024-05-13 16:40:14 -0500

    fix some bloc [sale, gps]

--- lib/src/domain/models/view.dart
+++ lib/src/domain/models/view.dart
@@ -1,12 +1,16 @@
 const String tableViews = 'app_views';
 
 class ViewFields {
-  static final List<String> values = [id, name, createdAt, appBlocId, route];
+  static final List<String> values = [
+    id,
+    name,
+    createdAt,
+    appBlocId,
+  ];
 
   static const String id = 'id';
   static const String name = 'name';
   static const String createdAt = 'createdAt';
-  static const String route = 'route';
   static const String appBlocId = 'appBlocId';
 }
 
@@ -15,33 +19,24 @@ class View {
   String? name;
   String? createdAt;
   bool? appBlocId;
-  String? route;
 
   View({
     this.id,
     this.name,
     this.createdAt,
     this.appBlocId,
-    this.route,
   });
 
   factory View.fromJson(Map<String, dynamic> json) => View(
-    id: json["id"],
-    name: json["name"],
-    createdAt: json["createdAt"],
-    appBlocId: json["appBlocId"] != null
-        ? json['appBlocId'] == 1
-        ? true
-        : false
-        : null,
-    route: json["route"],
-  );
+      id: json['id'],
+      name: json['name'],
+      createdAt: json['createdAt'],
+      appBlocId: json['appBlocId']);
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "name": name,
-    "createdAt": createdAt,
-    "appBlocId": appBlocId,
-    "route": route,
-  };
+        'id': id,
+        'name': name,
+        'createdAt': createdAt,
+        'appBlocId': appBlocId,
+      };
 }
