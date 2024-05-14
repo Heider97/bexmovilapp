ea66a232a8083669daba64c5882547e701ada6ce
Heider Zapa <heider.zapa@bexsoluciones.com>
2024-05-13 23:57:21 -0500

    fix some bloc [sale, gps]

--- lib/src/presentation/views/user/home/widgets/card_kpi.dart
+++ lib/src/presentation/views/user/home/widgets/card_kpi.dart
@@ -52,71 +52,26 @@ class _CardKpiState extends State<CardKpi> {
   }
 
   String buildContentKpi() {
-    if(widget.kpi.results is String ) {
-      return widget.kpi.results;
+    if (widget.kpi.results is String) {
+      if (widget.kpi.results.contains('/')) {
+        var splits = widget.kpi.results.split('/');
+        var result = [];
+
+        for (var split in splits) {
+          split = split is int || split is double ? split.toString() : split;
+          result.add(''.formattedCompact(split));
+        }
+
+        return result.join('/');
+      } else {
+        return widget.kpi.results;
+      }
+    } else if (widget.kpi.results is int) {
+      return ''.formatted(widget.kpi.results.toDouble());
+    } else if (widget.kpi.results is double) {
+      return ''.formattedCompact(widget.kpi.results);
     } else {
       return "N/A";
     }
-
-    // if (widget.needConverted == true && widget.kpi.results != null) {
-    //   if (widget.kpi.results is List && widget.kpi.results.isNotEmpty) {
-    //     if (widget.kpi.results.first is Kpi) {
-    //       return "N/A";
-    //     } else {
-    //       if ((widget.kpi.results.first['dato'] != null &&
-    //               widget.kpi.results.first['dato'] is String &&
-    //               widget.kpi.results.first['dato'].contains('/')) ||
-    //           (widget.kpi.results.first['y'] != null &&
-    //               widget.kpi.results.first['y'] is String &&
-    //               widget.kpi.results.first['y'].contains('/'))) {
-    //         var splits = [];
-    //         if (widget.kpi.results.first['dato'] != null) {
-    //           splits = widget.kpi.results.first['dato'].split('/');
-    //         } else if (widget.kpi.results.first['y'] != null) {
-    //           splits = widget.kpi.results.first['y'].split('/');
-    //         }
-    //         var result = [];
-    //         for (var split in splits) {
-    //           split =
-    //               split is int || split is double ? split.toString() : split;
-    //           result.add(''.formattedCompact(split));
-    //         }
-    //         return result.join('/');
-    //       } else {
-    //         var value = '';
-    //         if (widget.kpi.results.first['dato'] != null) {
-    //           value = widget.kpi.results.first['dato'] is double ||
-    //                   widget.kpi.results.first['dato'] is int
-    //               ? widget.kpi.results.first['dato'].toString()
-    //               : widget.kpi.results.first['dato'];
-    //         } else if (widget.kpi.results.first['y'] != null) {
-    //           value = widget.kpi.results.first['y'] is double ||
-    //                   widget.kpi.results.first['y'] is int
-    //               ? widget.kpi.results.first['y'].toString()
-    //               : widget.kpi.results.first['y'];
-    //         }
-    //
-    //         return ''.formattedCompact(value);
-    //       }
-    //     }
-    //   } else {
-    //     return "N/A";
-    //   }
-    // } else {
-    //   if (widget.kpi.results is List && widget.kpi.results.isNotEmpty) {
-    //     if (widget.kpi.results.first is Kpi) {
-    //       return "N/A";
-    //     } else {
-    //       var value = widget.kpi.results.first['dato'] is int
-    //           ? widget.kpi.results.first['dato'].toDouble()
-    //           : widget.kpi.results.first['dato'] is String
-    //               ? double.parse(widget.kpi.results.first['dato'])
-    //               : widget.kpi.results.first['dato'];
-    //       return ''.formatted(value);
-    //     }
-    //   } else {
-    //     return "N/A";
-    //   }
-    // }
   }
 }
--- lib/src/presentation/views/user/sale/features/products.dart
+++ lib/src/presentation/views/user/sale/features/products.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 //blocs
+import '../../../../../utils/constants/gaps.dart';
+import '../../../../../utils/constants/strings.dart';
 import '../../../../blocs/sale/sale_bloc.dart';
 
 //utils
@@ -10,6 +12,7 @@ import '../../../../../utils/constants/screens.dart';
 
 //widgets
 import '../../../../widgets/atoms/app_text.dart';
+import '../../../../widgets/atoms/app_text_button.dart';
 import '../../../../widgets/user/product_card.dart';
 import '../widgets/custom_card_product.dart';
 import '../widgets/custom_card_product_back.dart';
@@ -28,123 +31,87 @@ class SaleProducts extends StatefulWidget {
 }
 
 class _SaleProductsState extends State<SaleProducts> {
-  late SaleBloc saleBloc;
-
-  @override
-  void initState() {
-    saleBloc = BlocProvider.of<SaleBloc>(context);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    saleBloc.add(ResetStatus(status: SaleStatus.clients));
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     return BlocBuilder<SaleBloc, SaleState>(
         buildWhen: (previous, current) => previous.status != current.status,
         builder: (context, state) {
-          print(state.status);
-
           if (state.status == SaleStatus.products &&
               state.products != null &&
               state.products!.isNotEmpty) {
-            return Column(
-              children: [
-                Container(
-                  color: Colors.grey[200]!,
-                  height: Screens.height(context) * 0.77,
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      padding: EdgeInsets.zero,
-                      itemCount: state.products?.length,
-                      itemBuilder: (context, index) {
-                        List<Widget> carouselItems = [
-                          CustomCardProduct(product: state.products![index]),
-                          CustomCardProductBack(product: state.products![index])
-                        ];
-                        return CarouselSlider(
-                          options: CarouselOptions(
-                            height: MediaQuery.of(context).size.height * 0.22,
-                            enableInfiniteScroll: false,
-                            autoPlayInterval: const Duration(seconds: 4),
-                            aspectRatio: 2,
-                            enlargeCenterPage: false,
-                            scrollDirection: Axis.horizontal,
-                            autoPlay: false,
-                            viewportFraction: 1,
-                          ),
-                          items: carouselItems.map((item) {
-                            return Builder(
-                              builder: (BuildContext context) {
-                                return item;
-                              },
-                            );
-                          }).toList(),
-                        );
-                      }),
-                ),
-                /*   Material(
-                elevation: 10,
-                child: Container(
-                  height: 80,
-                  color: Colors.white,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            ' 0 productos',
-                            style: theme.textTheme.titleLarge!
-                                .copyWith(fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                      Row(children: [
-                        Text(
-                          'Vaciar',
-                          style: theme.textTheme.bodyMedium!
-                              .copyWith(color: theme.primaryColor),
-                        ),
-                        gapW20,
-                        InkWell(
-                          onTap: () {
-                            _navigationService.goTo(AppRoutes.shoppingCart);
-                          },
-                          child: SizedBox(
-                            height: 40,
-                            child: Material(
-                              color: theme.primaryColor,
-                              borderRadius: BorderRadius.circular(10),
-                              elevation: 5,
-                              child: Center(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 15.0, right: 15),
-                                  child: Text(
-                                    'Ver Carrito',
-                                    style: theme.textTheme.bodyMedium!.copyWith(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ),
+            return Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          padding: EdgeInsets.zero,
+                          itemCount: state.products?.length,
+                          itemBuilder: (context, index) {
+                            List<Widget> carouselItems = [
+                              CustomCardProduct(
+                                  product: state.products![index]),
+                              CustomCardProductBack(
+                                  product: state.products![index])
+                            ];
+                            return CarouselSlider(
+                              options: CarouselOptions(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.22,
+                                enableInfiniteScroll: false,
+                                autoPlayInterval: const Duration(seconds: 4),
+                                aspectRatio: 2,
+                                enlargeCenterPage: false,
+                                scrollDirection: Axis.horizontal,
+                                autoPlay: false,
+                                viewportFraction: 1,
                               ),
+                              items: carouselItems.map((item) {
+                                return Builder(
+                                  builder: (BuildContext context) {
+                                    return item;
+                                  },
+                                );
+                              }).toList(),
+                            );
+                          })),
+                  Material(
+                      elevation: 10,
+                      child: Container(
+                        height: 60,
+                        color: Colors.white,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                AppText(
+                                  ' 0 Productos',
+                                ),
+                              ],
                             ),
-                          ),
-                        )
-                      ])
-                    ],
-                  ),
-                )) */
-              ],
+                            Row(children: [
+                              AppText('Vaciar'),
+                              gapW20,
+                              SizedBox(
+                                  height: 40,
+                                  child: AppTextButton(
+                                      child: AppText('Ver Carrito'),
+                                      onPressed: () {
+                                        _navigationService
+                                            .goTo(AppRoutes.cartSale);
+                                      })),
+                            ])
+                          ],
+                        ),
+                      ))
+                ],
+              ),
             );
           } else {
             return Center(child: AppText('No hay Productos'));
